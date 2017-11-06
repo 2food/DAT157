dd4be8d0108f31506213d6bd973eb37cd7a5cc91
Tormod Mathiesen <tormathi@gmail.com>
2017-11-06 15:27:35 +0100

    done with oppg 2

--- Oppgave2/kohonen_square.py
+++ Oppgave2/kohonen_square.py
@@ -6,17 +6,19 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    size = 10
+    print("Network size:")
+    rowsize = int(input("Row: "))
+    colsize = int(input("Column: "))
 
     nodes = []
-    for y in range(size):
+    for y in range(rowsize):
         line = []
-        for x in range(size):
+        for x in range(colsize):
             line.append([0.5 + (x-5) / 1000, 0.5 - (y-5) / 1000])
         nodes.append(line)
 
-    alpha = 0.2
-    neighbours = 5
+    alpha = float(input("Initial alpha [0.1, 0.3]: "))
+    neighbours = int(input("Initial neighbours: "))
     iterations = 1000
 
     nodes = learn(alpha, neighbours, nodes, iterations)
@@ -40,7 +42,7 @@ def winner(nodes, inn):
     x, y = inn
     minrow, mincol = 0, 0
     for row in range(len(nodes)):
-        for col in range(len(nodes)):
+        for col in range(len(nodes[0])):
             thisdiff = np.sqrt((x - nodes[row][col][0]) ** 2 + (y - nodes[row][col][1]) **2)
             mindiff = np.sqrt((x - nodes[minrow][mincol][0]) ** 2 + (y - nodes[minrow][mincol][1]) **2)
             if thisdiff < mindiff:
@@ -51,6 +53,7 @@ def winner(nodes, inn):
 
 def learn(al0, neigh0, nodes, it):
     for i in range(it):
+        # Alpha og naboskap avtar lineært
         al = al0 * (1-(i/it))
         neigh = np.ceil(neigh0 * (1-(i/it)))
         if i % 50 == 0:
@@ -61,7 +64,7 @@ def learn(al0, neigh0, nodes, it):
         winrow, wincol = winner(nodes, (x, y))
         for row in range(len(nodes)):
             if winrow - neigh <= row <= winrow + neigh:
-                for col in range(len(nodes)):
+                for col in range(len(nodes[0])):
                     if wincol - neigh <= col <= wincol + neigh:
                         nodes[row][col][0] += al * (x - nodes[row][col][0])
                         nodes[row][col][1] += al * (y - nodes[row][col][1])
